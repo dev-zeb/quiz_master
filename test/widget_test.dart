eab773d1d7fa127f65b945fbd89b296bed583709
Sufi Aurangzeb Hossain <azhossain.98.ah@gmail.com>
2025-03-08 22:05:40 +0600

    Rename the widget name in widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:learn_and_quiz/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Quiz());
+    await tester.pumpWidget(const QuizMaster());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
