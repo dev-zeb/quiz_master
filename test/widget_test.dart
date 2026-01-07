2d9e0f361ccd5331438f627f9cdca0874316f816
Sufi Aurangzeb Hossain <azhossain.98.ah@gmail.com>
2026-01-07 22:09:28 +0600

    Fix an error in the widget_test.dart file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:quiz_master/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const QuizMaster());
+    await tester.pumpWidget(const QuizMasterApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
