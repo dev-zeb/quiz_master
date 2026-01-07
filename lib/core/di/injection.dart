8db4270e47a265fbebe4d4ec90040f9b6b28c6bc
Sufi Aurangzeb Hossain <azhossain.98.ah@gmail.com>
2026-01-08 00:44:46 +0600

    Add the dependency for ai_quiz

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -3,23 +3,26 @@ import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 import 'package:http/http.dart' as http;
 
-import '../../features/auth/data/datasources/firebase_auth_data_source.dart';
-import '../../features/auth/data/repositories/auth_repository_impl.dart';
-import '../../features/auth/domain/repositories/auth_repository.dart';
-import '../../features/auth/presentation/bloc/auth_bloc.dart';
-import '../../features/quiz/data/datasources/local/quiz_local_data_source.dart';
-import '../../features/quiz/data/datasources/remote/ai_quiz_remote_data_source.dart';
-import '../../features/quiz/data/datasources/remote/quiz_remote_data_source.dart';
-import '../../features/quiz/data/models/quiz_history_model.dart';
-import '../../features/quiz/data/models/quiz_model.dart';
-import '../../features/quiz/data/repositories/ai_quiz_repository_impl.dart';
-import '../../features/quiz/data/repositories/quiz_repository_impl.dart';
-import '../../features/quiz/domain/repositories/ai_quiz_repository.dart';
-import '../../features/quiz/domain/repositories/quiz_repository.dart';
-import '../../features/quiz/presentation/bloc/quiz_bloc.dart';
-import '../config/strings.dart';
-import '../config/theme/bloc/theme_bloc.dart';
-import '../config/theme/theme_prefs.dart';
+import 'package:quiz_master/core/config/strings.dart';
+import 'package:quiz_master/core/config/theme/bloc/theme_bloc.dart';
+import 'package:quiz_master/core/config/theme/theme_prefs.dart';
+
+import 'package:quiz_master/features/auth/data/datasources/firebase_auth_data_source.dart';
+import 'package:quiz_master/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:quiz_master/features/auth/domain/repositories/auth_repository.dart';
+import 'package:quiz_master/features/auth/presentation/bloc/auth_bloc.dart';
+
+import 'package:quiz_master/features/quiz/data/datasources/local/quiz_local_data_source.dart';
+import 'package:quiz_master/features/quiz/data/datasources/remote/ai_quiz_remote_data_source.dart';
+import 'package:quiz_master/features/quiz/data/datasources/remote/quiz_remote_data_source.dart';
+import 'package:quiz_master/features/quiz/data/models/quiz_history_model.dart';
+import 'package:quiz_master/features/quiz/data/models/quiz_model.dart';
+import 'package:quiz_master/features/quiz/data/repositories/ai_quiz_repository_impl.dart';
+import 'package:quiz_master/features/quiz/data/repositories/quiz_repository_impl.dart';
+import 'package:quiz_master/features/quiz/domain/repositories/ai_quiz_repository.dart';
+import 'package:quiz_master/features/quiz/domain/repositories/quiz_repository.dart';
+import 'package:quiz_master/features/quiz/presentation/bloc/quiz_bloc.dart';
+import 'package:quiz_master/features/quiz/presentation/bloc/ai_quiz_bloc.dart';
 
 final getIt = GetIt.instance;
 
@@ -50,10 +53,6 @@ Future<void> configureDependencies() async {
   );
 
   // Hive local datasource
-  //
-  // IMPORTANT:
-  // These boxes MUST be opened in initializeHive() and adapters registered.
-  // If QuizModel / QuizHistoryModel are Hive objects, their TypeAdapters must be registered too.
   final quizBox = Hive.box<QuizModel>(AppStrings.quizBoxName);
   final quizHistoryBox =
       Hive.box<QuizHistoryModel>(AppStrings.quizHistoryBoxName);
@@ -83,4 +82,8 @@ Future<void> configureDependencies() async {
       authRepository: getIt<AuthRepository>(),
     ),
   );
+
+  // AI quiz generation bloc
+  getIt
+      .registerFactory<AiQuizBloc>(() => AiQuizBloc(getIt<AiQuizRepository>()));
 }
